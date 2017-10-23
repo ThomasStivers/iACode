ca22d82ad6c0912f0ec9d4a4a6e1be75c2ddaf6b
ThomasStivers <thomas.stivers@gmail.com>
2017-10-23 04:51:58 -0500

    Moved labeller into a folder. Made extensive changes to labeller.

--- labeller/labeller.py
+++ labeller/labeller.py
@@ -12,9 +12,10 @@ parser.add_argument("-V", "--version", action="version", version="2017.10")
 
 class Label:
 
-	def __init__(self, aisle, bay, level, slot, building, separator="-"):
+	def __init__(self, type=None, aisle=None, bay=None, level=None, slot=None, building=None, separator="-"):
 		self.building = building
 		self.separator = separator
+		self.type = type
 		self.aisle = aisle
 		self.bay = bay
 		self.level = level
@@ -22,15 +23,18 @@ class Label:
 
 	def __str__(self):
 		if self.building == "TLR":
-			template = "{prefix}{separator}{aisle:02d}{separator}{bay:02d}{separator}{level}{separator}{slot:02d}"
-			return template.format(
-				prefix=self.building,
-				separator=self.separator,
-				aisle=self.aisle,
-				bay=self.bay,
-				level=self.level,
-				slot=self.slot
-			)
+			template = "{building}{separator}{aisle:02d}{separator}{bay:02d}{separator}{level}{separator}{slot:02d}"
+		elif self.building in ("AF", "402", "MC", "225", "BULK", "220"):
+			template = "{building}{separator}{type}{separator}{aisle:02d}{separator}{bay:02d}{separator}{slot}"
+		return template.format(
+			building=self.building,
+			separator=self.separator,
+			level=self.level,
+			type=self.type,
+			aisle=self.aisle,
+			bay=self.bay,
+			slot=self.slot
+		)
 
 class Labels(list):
 	def __init__(self, columns=6):
@@ -77,6 +81,55 @@ class Labels(list):
 							if ("regexp" in locals()) and (regexp.search(str(l)) == None):
 								continue
 							self.append(l)
+		if building in ("AF", "402"):
+			floor = "F"
+			rack = "R"
+			shelf = "S"
+			mezannine = "M"
+			pallets = [floor, rack]
+			boxes = [shelf, mezannine]
+			building = "402"
+			maxAisle = 28 # There are no more than 28 aisles in the building.
+			minBay = 0
+			maxBay = 40 # there are never more than 40 bays on an aisle.
+			types = ["F", "M", "R", "S"] # The types of location at AF.
+			slots = list("ABCDEFGH") # The highest slot letter encountered at AF.
+
+			for type in types: # Loop over all the location types.
+				if type == floor: # Floor locations only have slots A and B.
+					slots = list("AB")
+				if type in pallets:
+					maxAisle = 23
+					maxBay = 18
+				elif type in boxes:
+					maxAisle=28
+				for aisle in xrange(0,maxAisle+1): # For each aisle loop over all the bays.
+					if type in boxes:
+						slots = list("ABCDEF")
+						minBay = 1
+						if aisle == 0:
+							maxBay = 23
+							slots = list("ABCD")
+						elif aisle in [1, 2]: maxBay = 25
+						elif aisle in xrange(3, 16): maxBay = 11
+						if aisle in [17, 18]: maxBay = 25
+						elif aisle == 19: maxBay = 12
+						elif aisle in xrange(20, 27): continue
+						elif aisle in [27, 28]: maxBay = 40
+						if aisle == 27: slots = list("ABCD")
+						if aisle == 28: slots = list("ABC")
+					for bay in xrange(minBay, maxBay+1): # For each bay loop over all the levels.
+						if type in boxes:
+							if bay == 0: continue
+							if type == "M" and aisle == 0: continue
+						for slot in slots: # For each type of each bay on each aisle loop over all slots.
+							if type in ["F", "R"] and bay == 17: # There are floor level tunnels at bays 20 and 33 that are 2 levels high.
+								continue
+							l = Label(type=type, aisle=aisle, bay=bay, slot=slot, building=building)
+							if ("regexp" in locals()) and (regexp.search(str(l)) == None):
+								continue
+							self.append(l)
+
 
 if __name__ == "__main__":
 	args = parser.parse_args()
